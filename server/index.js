aaf660148fbd97a1b1d056c51b1e97c2bc6ecee5
Coderofcoders70 <Lakshayas918@gmail.com>
2026-01-16 01:28:17 +0530

    Updated index file

--- server/index.js
+++ server/index.js
@@ -10,8 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173" 
+  origin: [
+    "http://localhost:5173", 
+    "https://me-api-playground-nine-pi.vercel.app" 
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
+
 app.use(express.json()); 
 app.use('/api/profile', profileRoutes);
 
